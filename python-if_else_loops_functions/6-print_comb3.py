c982fc9104302be6c42cc77d62f70fa990f54427
NadegeL <nadege.luthier@gmail.com>
2024-05-11 23:11:38 +0200

    enfin range 8!

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
+for i in range(8):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i + j), end=", ")
+        print("{}{}".format(i,j), end=", ")
 print("89")
         
