4890764a7227dd43c30ce6068c309bef363b5629
NadegeL <nadege.luthier@gmail.com>
2024-05-11 22:58:59 +0200

    c'est pas encore ça

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{:d}{:d}, ".format(i, j), end="")
-print("99")
+        print("{:02d}".format(i + j), end=", ")
+print("89")
+        
