7ea91f077ffd78f412ed1d5c1baac2e8482f537e
NadegeL <nadege.luthier@gmail.com>
2024-05-13 13:29:17 +0200

    autre méthode

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    # Récupère le nombre d'arguments passés au programme
-    num_args = len(sys.argv) -1
-    print("number of argument(s)", num_args, end = "")
-    if num_args == 1:
-        print("followed by argument:")
-    elif num_args >= 1:
-        print("followed by arguments:")
-    else:
-        print(".",)
-# Imprime chaque argument avec sa position   
-for i in range(1, len(sys.argv)):
-    print(i, ":",sys.argv[i])
+    
+    argv = sys.argv[1:]
+if len(argv) == 0:
+    print("{} arguments.".format(len(argv)))
+elif len(argv) == 1:
+    print("{} argument:".format(len(argv)))
+else:
+    print("{} arguments:".format(len(argv)))
+
+i = 1
+while i <= len(argv):
+    print("{}: {}".format(i, sys.argv[i]))
+    i += 1
