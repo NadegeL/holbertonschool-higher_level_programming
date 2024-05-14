ebedf78157e50fd0a845a97e6f982e4727b7d301
NadegeL <nadege.luthier@gmail.com>
2024-05-14 14:48:47 +0200

    retour à la ligne

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print({""}.format(matrix))
+    if matrix == 0:
+        print({"{}"}.format(matrix))
     else:
         for row in matrix:
             for element in row:
-                print("{:d}".format(element), end="")
+                print("{:d}".format(element))
+            print()
+        
