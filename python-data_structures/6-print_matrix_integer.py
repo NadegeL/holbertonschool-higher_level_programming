4990b6d528c1f1fd441d4e10d5fa6109c86a2a3e
NadegeL <nadege.luthier@gmail.com>
2024-05-14 14:52:04 +0200

    .join

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix == 0:
-        print({"{}"}.format(matrix))
+        print({""}.format(matrix))
     else:
         for row in matrix:
-            for element in row:
-                print("{:d}".format(element))
-            print()
+            print(" ".join(map(str,row)))
         
