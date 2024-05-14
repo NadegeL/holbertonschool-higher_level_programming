f9412105ad91e718f1ed11468d06535d83d8874a
NadegeL <nadege.luthier@gmail.com>
2024-05-14 22:41:24 +0200

    using "padding"

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Remplir les tuples avec des zéros pour les valeurs manquantes
+    tuple_a_padded = tuple_a + (0, ) * (2 - len(tuple_a))
+    tuple_b_padded = tuple_b + (0, ) * (2 - len(tuple_b))
+    
+    # Utiliser uniquement les deux premiers éléments
+    final_tuple = (tuple_a_padded[0] + tuple_b_padded[0], 
+                   tuple_a_padded[1] + tuple_b_padded[1])
+    
+    return final_tuple
