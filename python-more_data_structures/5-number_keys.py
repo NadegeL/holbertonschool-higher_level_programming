04f3725ba3520c249af4902fbd4ab1ccecf7e8fc
NadegeL <nadege.luthier@gmail.com>
2024-05-15 23:28:59 +0200

    number keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,7 +2,7 @@
 def number_keys(a_dictionary):
     count = 0
     for key in a_dictionary:
-        count +=1
+        count += 1
         if isinstance(a_dictionary[key], dict):
             count += number_keys(a_dictionary)[key]
     return count
