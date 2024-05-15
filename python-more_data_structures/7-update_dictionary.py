4a24c0cf7f4ae0cf9204949b418928299168b6ef
NadegeL <nadege.luthier@gmail.com>
2024-05-15 23:50:17 +0200

    remplace plutot qu'ajoute

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    
-    update_dict = {**a_dictionary, key: value}
-    return update_dict
+
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
