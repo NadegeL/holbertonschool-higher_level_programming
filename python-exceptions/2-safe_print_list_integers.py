4962f5b9906096c7108068fc469318181d5b611c
NadegeL <nadege.luthier@gmail.com>
2024-05-16 22:40:01 +0200

    list division

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[idx]), end="")
                 element += 1
     except ValueError:
-       pass
-    print()
+    pass
+	print()
     return element
