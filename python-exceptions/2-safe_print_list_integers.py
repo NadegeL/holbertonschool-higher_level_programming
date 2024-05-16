8e92ff6b772836335eaf78e04dd0a940166ef86b
NadegeL <nadege.luthier@gmail.com>
2024-05-16 15:50:12 +0200

    safe print list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[idx], int):
                 print("{:d}".format(my_list[idx]), end="")
                 element += 1
-    except (TypeError, ValueError):
+    except ValueError:
        pass
     print()
     return element
