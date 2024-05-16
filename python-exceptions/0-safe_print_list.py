7d7694b919234cdf14a87ed28b8a251a53111a45
NadegeL <nadege.luthier@gmail.com>
2024-05-16 15:00:42 +0200

    safe-print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-    while idx < x:
-        try:
-            print("{:d}".format(my_list[idx]), end =" ")
-        except IndexError:
-            idx += 1
+    element = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            element += 1
+    except IndexError:
+        pass
     print()
+    return element
