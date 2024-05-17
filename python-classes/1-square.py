33fdf6bd75231431bb1d4bb5787336c5f47a7921
NadegeL <nadege.luthier@gmail.com>
2024-05-17 11:34:44 +0200

    private attribut size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square"""
+""" with private attribut size"""
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
