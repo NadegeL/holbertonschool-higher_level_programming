900d3ab755c5357a4432455efbadc5cfab94d753
NadegeL <nadege.luthier@gmail.com>
2024-05-21 13:00:53 +0200

    docu

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+Write a function that prints My name is <first name> <last name>
+"""
 
 
 def say_my_name(first_name, last_name=""):
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name,str):
