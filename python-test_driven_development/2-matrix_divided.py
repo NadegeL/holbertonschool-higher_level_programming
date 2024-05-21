660ea7eda086e71f10d39308e2fc7d24052b84f7
NadegeL <nadege.luthier@gmail.com>
2024-05-21 09:05:50 +0200

    test exceptions

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a function that divides all elements of a matrix.
+"""
+def matrix_divided(matrix, div):
+   
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(elem, (int, float)) for row in matrix for elem in row):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    row_lengths = [len(row) for row in matrix]
+    if len(set(row_lengths)) != 1:
+        raise TypeError("Each row of the matrix must have the same size")
+        
+    if not isinstance(div(int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    matrice = [[round(elem / div, 2)for elem in row] for row in matrix]
+
+    return matrice
+
+
+if__name__ = "__main__"
+import doctest
+doctest.testfile(test/2-matrix_divided.py)
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+trying to test make some testfiles with Doctest
+documentation (docstrings)
+All your tests should be executed by using this command: python3 -m doctest ./tests/*
