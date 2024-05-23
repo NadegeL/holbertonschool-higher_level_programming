28f0374bb82f8304cda4769eef4707bcbce396e3
NadegeL <nadege.luthier@gmail.com>
2024-05-23 22:11:33 +0200

    other symbol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -56,7 +56,8 @@ class Rectangle:
         if self.__width == 0 or self.height == 0:
             return ""
 
-        lines = [str(self.print_symbol) * self.__width for _ in range(self.height)]
+        lines = [str(self.print_symbol) * self.__width for _ in range
+                 (self.height)]
         return "\n".join(lines)
 
     def __repr__(self):
