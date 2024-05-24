0e1ffc431bf00f6fd7ea2128310722108d0ab5e3
NadegeL <nadege.luthier@gmail.com>
2024-05-24 14:55:55 +0200

    issubclass , type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,7 +8,4 @@ is an instance of a class that inherited
 def inherits_from(obj, a_class):
     """returns True or False"""
     
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return issubclass(obj.__class__, a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
