6e333f2bd1f81da75f740c3b8db74148387df448
NadegeL <nadege.luthier@gmail.com>
2024-05-24 14:57:22 +0200

    pep8

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,5 +7,5 @@ is an instance of a class that inherited
 
 def inherits_from(obj, a_class):
     """returns True or False"""
-    
+
     return issubclass(type(obj), a_class) and type(obj) != a_class
