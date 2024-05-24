80919cc21a693110efa80ea08547f41d5b1ac769
NadegeL <nadege.luthier@gmail.com>
2024-05-24 13:40:27 +0200

    utiliser type

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """verify obj is exactly instance"""
+
+
 def is_same_class(obj, a_class):
+    """
+        Write a function that returns True
+        otherwise False.
+    """
     return type(obj) is a_class
+
 
 is_same_class(1, int)
 is_same_class(1, object)
