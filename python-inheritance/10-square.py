fe8447246bc31f33cf0d14dd69e8086401184cae
NadegeL <nadege.luthier@gmail.com>
2024-05-25 15:37:11 +0200

    documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,9 +6,12 @@ class Square(Rectangle):
     """write a subclass Square"""
 
     def __init__(self, size):
+        """method to initialize square
+        and use super() function
+        """
+        super().__init__(size, size)
         self.__size = size
         self.integer_validator("size", size)
-        super().__init__(size, size)
 
     def area(self):
         return self.__size ** 2
