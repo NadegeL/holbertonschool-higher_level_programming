cdafb68f182f961919e065099dc77d1bdb095166
NadegeL <nadege.luthier@gmail.com>
2024-05-25 08:11:32 +0200

    instance method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """it is an empty class"""
+
+    def area(self):
+        """instance method"""
+        raise Exception("area() is not implemented")
