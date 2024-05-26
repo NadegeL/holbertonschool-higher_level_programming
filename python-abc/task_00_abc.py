271661b27893bbdaa99ecbe30477bb4a2b617b10
NadegeL <nadege.luthier@gmail.com>
2024-05-26 22:09:54 +0200

    0. Abstract Animal Class and its Subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    Defines the class
+    """
+    @abstractmethod
+    def sound(self):
+        """
+    abstract method
+        """
+        pass
+
+class Dog(Animal):
+    """
+    Dog class inherits from Animal
+    """
+    def sound(self):
+        """
+        Method that return the string Bark
+        """
+        return "Bark"
+
+class Cat(Animal):
+    """
+    Cat class inherits from Animal
+    """
+    def sound(self):
+        """
+        Method that return the string Meow
+        """
+        return "Meow"
