5a7ba830a94a470fb073bdd340b4d4f3bdeb1d0e
NadegeL <nadege.luthier@gmail.com>
2024-05-28 14:30:40 +0200

    task 00

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Create a basic serialization module
+deserialize the JSON file to recreate the Python Dictionary.
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+
+    with open(filename, 'r') as f:
+        json.loads(f)
