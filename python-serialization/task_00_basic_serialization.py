9a0bce2561a54002923688c5ae361544320a8bc1
NadegeL <nadege.luthier@gmail.com>
2024-05-28 14:44:13 +0200

    return!!!!!!

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -8,12 +8,12 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-
+    """serialize with dump"""
     with open(filename, 'w') as f:
         json.dump(data, f)
 
 
 def load_and_deserialize(filename):
-
+    """deserialize with load"""
     with open(filename, 'r') as f:
-        json.load(f)
+        return json.load(f)
