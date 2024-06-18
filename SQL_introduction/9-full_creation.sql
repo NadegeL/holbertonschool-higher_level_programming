0ff691a542629ba49788cf4ac0e9117391cebe72
NadegeL <nadege.luthier@gmail.com>
2024-06-18 12:36:28 +0200

    PROJET 1 sans tests

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+ -- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+
+CREATE TABLE second_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    score INT NOT NULL,
+    PRIMARY KEY (id)
