18738d035ade0bf332f904b5f12be4b78cdfaede
NadegeL <nadege.luthier@gmail.com>
2024-06-19 19:32:24 +0000

    11, 14, 15 corrigés

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
 --lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
 
-SELECT score, COUNT(score) FROM second_table GROUP BY score;
+SELECT score, COUNT(score) 
+FROM second_table 
+ORDER BY score DESC;
