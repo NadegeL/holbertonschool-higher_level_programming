edb7807b23b1fe6a99de19e62a9fb49f246e586d
NadegeL <nadege.luthier@gmail.com>
2024-06-19 19:16:09 +0000

    IF NOT EXISTS arguments

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
  -- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     score INT NOT NULL,
     PRIMARY KEY (id)
+);
