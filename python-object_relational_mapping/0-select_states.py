47914d8030d26f8dec35e533b8119be95baa7989
NadegeL <nadege.luthier@gmail.com>
2024-06-22 22:30:28 +0200

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+""" 
+a script that lists all states from the database hbtn_0e_0_usa
+take 3 arguments: mysql username, mysql password and database name
+use the module MySQLdb (import MySQLdb)
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
