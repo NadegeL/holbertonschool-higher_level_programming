52b02c87acebcd20a0c6accb2161e328ccee7d75
NadegeL <nadege.luthier@gmail.com>
2024-06-23 16:05:59 +0200

    dernier essaie

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,12 @@ script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument
 """
 
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    # Connexion à la base de données
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -17,20 +18,13 @@ if __name__ == '__main__':
         passwd=argv[2],
         db=argv[3]
     )
-
-    # Créer un curseur
     cur = db.cursor()
-
-    # Utilisation de paramètres pour la requête pour éviter les injections SQL
-    query = ("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
                 ORDER BY states.id ASC".format(argv[4]))
-    cur.execute(query, (argv[4],))
-
-    # Récupération des résultats
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
 
-    # Fermeture du curseur et de la connexion à la base de données
     cur.close()
     db.close()
