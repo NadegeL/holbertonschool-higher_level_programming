0e9122a6e7f5f583d67d8fc566b4bccb2d3c191f
NadegeL <nadege.luthier@gmai.com>
2024-09-03 21:16:44 +0200

    corrections task

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number // 10 < number % 10:
-        if number != 89:
-            print("{:02d}".format(number), end=", ")
-        else:
-            print("{:02d}".format(number))
+for i in range(8):
+    for j in range(i + 1, 10):
+        print("{}{}".format(i, j), end=", ")
+print("89")
