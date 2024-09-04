f830e08c093250b1473abbab1c4529f17548c758
NadegeL <nadege.luthier@gmai.com>
2024-09-04 14:43:28 +0200

    argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
-        print("1:{}".format(sys.argv[1]))
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(count))
         for i in range(1, count + 1):
