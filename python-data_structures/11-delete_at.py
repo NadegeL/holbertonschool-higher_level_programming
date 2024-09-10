b8b52bbd6d60c5bfc908249b56cdd8a653c6fd62
NadegeL <nadege.luthier@gmai.com>
2024-09-10 17:27:36 +0200

    corrections

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,5 +4,5 @@ def delete_at(my_list=[], idx=0):
         if idx < 0 or idx >= len(my_list):
             return my_list
         else:
-            del my_list[idx -3]
+            del my_list[3]
             return my_list
