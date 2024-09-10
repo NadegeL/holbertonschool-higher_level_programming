01d329db748ea41ecf34e685e270a60bc87ffaac
NadegeL <nadege.luthier@gmai.com>
2024-09-10 17:10:25 +0200

    switch

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    for idx in my_list:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            del my_list[idx -3]
+            return my_list
