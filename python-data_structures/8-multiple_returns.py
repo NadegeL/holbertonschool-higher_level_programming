e08234e43dd327c6112292383444071ef7e18b73
NadegeL <nadege.luthier@gmai.com>
2024-09-10 17:19:37 +0200

    pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
     return (length, first_char)
-#on calcule d'abord la longueur puis on retourne le premier caractère de la phrase
+# on calcule d'abord la longueur puis
+# on retourne le premier caractère de la phrase
