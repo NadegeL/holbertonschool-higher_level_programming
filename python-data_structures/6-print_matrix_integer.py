57780b144de049dbcf5cc986a08d1c690e3aa930
NadegeL <nadege.luthier@gmai.com>
2024-09-10 17:17:38 +0200

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
     for row in matrix:
         for element in row:
             print("{}".format(element), end=" ")
