c9c2fc2279603c5e99fe8e19cb746beb6109fd09
NadegeL <nadege.luthier@gmai.com>
2024-09-16 10:35:30 +0200

    property size, setter.size

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this is class Square that defines size"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """size in private instance attribute"""
+        while not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
+
+    def area(self):
+        """public instance return current square area"""
+        return self._size ** 2
