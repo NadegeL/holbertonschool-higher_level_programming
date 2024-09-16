13d6a866429a646074516ed1f11db605680a27f3
NadegeL <nadege.luthier@gmai.com>
2024-09-16 12:15:08 +0200

    private size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,4 +5,4 @@ class Square:
     """ this is class Square that defines size"""
     def __init__(self, size):
         """size in private instance attribute"""
-        self._size = size
+        self.__size = size
