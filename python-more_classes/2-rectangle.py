0ddefb027289165412b3e66dc60b1feef9c0295f
NadegeL <nadege.luthier@gmai.com>
2024-09-18 14:13:18 +0200

    corriger indentation

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -8,23 +8,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Method that initializes the instance"""
-        self.__height = height
         self.__width = width
-     
-        
-    @property
-    def height(self):
-        """Method that returns the value of the height"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Method that returns the value of the height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
 
     @property
     def width(self):
@@ -39,3 +24,17 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """Method that returns the value of the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Method that returns the value of the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
