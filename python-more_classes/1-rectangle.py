7f8360c5a41f323a74f0f7bfac9ae7fc4ccacf11
NadegeL <nadege.luthier@gmai.com>
2024-09-18 13:56:11 +0200

    corriger indentation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,32 +11,30 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        @property
-        def width(self):
-            """Method that returns the value of the width"""
-            return self.__width
+    @property
+    def width(self):
+        """Method that returns the value of the width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Method that returns the value of the width"""
-            while i in value:
-                if not isinstance(i, int):
-                    raise TypeError("width must be an integer")
-                if i < 0:
-                    raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Method that returns the value of the width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """Method that returns the value of the height"""
-            return self.__height
+    @property
+    def height(self):
+        """Method that returns the value of the height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """Method that returns the value of the height"""
-            while i in value:
-                if not isinstance(i, int):
-                    raise TypeError("height must be an integer")
-                if i < 0:
-                    raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Method that returns the value of the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
