e50b6ca5fa2e3910002236d8475fb995642c8d11
NadegeL <nadege.luthier@gmai.com>
2024-09-20 15:23:05 +0200

    print_symbol in str.join

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -57,7 +57,9 @@ class Rectangle:
         """Method to print a string representation"""
         if self.__height == 0 or self.__width == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
+        return "\n".join(
+            [str(self.print_symbol) * self.__width] * self.__height
+        )
 
     def my_print(self):
         """Method to print with # """
