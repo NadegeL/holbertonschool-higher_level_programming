b023986838cfbe874d9e42ce685ed85bdf48d674
NadegeL <nadege.luthier@gmai.com>
2024-09-23 13:52:37 +0200

    Task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""write a class square that inherits from rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherits from rectangle"""
+
+    def __init__(self, size):
+        """method to initialize square
+        and use super() function
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Public instance method"""
+        self.area = self.__size ** 2
+        return self.area
+
+    def __str__(self):
+        """should return a string"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def print(self):
+        """should print"""
+        print("[Square] {}/{}".format(self.__size, self.__size))
