4fdc41ed64cfa4fdcd0b2857f0585c333c935e6f
NadegeL <nadege.luthier@gmai.com>
2024-09-23 13:34:42 +0200

    method "super saiyan"

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""write a class square that inherits from rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class square that inherits from rectangle"""
+
+    def __init__(self, size):
+        """method to initialize square
+        and use super() function
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """Public instance method"""
+        self.area = self.__size ** 2
+        return self.area
---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that inherits from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialisation a rectangle
+            with integer_validator
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Public instance method:"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """should return a string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def print(self):
+        """should print"""
+        print("[Rectangle] {}/{}".format(self.__width, self.__height))
