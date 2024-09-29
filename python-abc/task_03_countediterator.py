3df45ed7ae6f4b417c95822fd6f160b743297d3e
NadegeL <nadege.luthier@gmai.com>
2024-09-29 15:15:45 +0200

    iterator

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-"""Create a class named CountedIterator that extends the built-in iterator class."""
+"""Create a class named CountedIterator
+that extends the built-in iterator class."""
 
 
 class CountedIterator:
-    def __init__(self, index):
-        self.index = index
-        self
+    """class CountedIterator"""
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        """method __iter__"""
+        return self
+
+    def __next__(self):
+        """method __next__"""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def get_count(self):
+        """method get_count"""
+        return self.count
