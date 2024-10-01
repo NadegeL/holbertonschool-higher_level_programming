80cffa9d23e97b8635514ded8ce5a676ca13f7f9
NadegeL <nadege.luthier@gmai.com>
2024-10-01 15:07:50 +0200

    task 1 nb char

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ and returns the number of characters written"""
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)"""
     with open(filename, 'w', encoding='UTF-8') as file:
-        file.write = text
+        nb_char = file.write(text)
         return len(text)
