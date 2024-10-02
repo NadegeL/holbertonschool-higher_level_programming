55dd8b758eaf17922857edc1364a288a97129948
NadegeL <nadege.luthier@gmai.com>
2024-10-02 14:41:44 +0200

    task revu

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,11 +7,4 @@ for JSON serialization of an object:"""
 
 def class_to_json(obj):
     """verify instance obj"""
-    if isinstance(obj, dict):
-        return {k: obj.__dict__[k] for k in obj.__dict__ if k in obj.__dict__}
-    elif isinstance(obj, list):
-        return [class_to_json(i) for i in obj]
-    elif isinstance(obj[str, bool, int]):
-        return obj
-    else:
-        return obj.__dict__
+    return obj.__dict__
---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Create a basic serialization module"""
+
+
+def serialize_and_save_to_file(data, filename):
+    
