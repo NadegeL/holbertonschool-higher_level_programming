3d397c47a7b85b914385a6cd9610e81090a3b8c9
NadegeL <nadege.luthier@gmai.com>
2024-10-25 15:05:35 +0200

    TESTS

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0.ABORT.
 
-SHOW TABLES IN second_table;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,12 @@
--- create a second table in hbtn_0c_0.ABORT
+-- Script that creates a table second_table
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+    );
 
-CREATE TABLE IF NOT EXISTS second_table IN hbtn_0c_0 (
-id INT,
-name VARCHAR(256),
-score INT
-);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
