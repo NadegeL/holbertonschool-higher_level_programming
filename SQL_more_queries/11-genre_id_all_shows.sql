0493d26dfa2b45f7985e95f7fbcb5758cd99df11
NadegeL <nadege.luthier@gmai.com>
2024-10-30 11:47:32 +0100

    NOT NULL

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows contained in the database hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
