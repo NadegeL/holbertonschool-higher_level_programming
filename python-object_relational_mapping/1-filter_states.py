f2b8bf57877b5a3d5aa2e9ec93008f7d7645fc06
NadegeL <nadege.luthier@gmai.com>
2024-10-31 16:02:30 +0100

    pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 from sys import argv
@@ -14,7 +15,8 @@ if __name__ == '__main__':
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE \
+                'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
