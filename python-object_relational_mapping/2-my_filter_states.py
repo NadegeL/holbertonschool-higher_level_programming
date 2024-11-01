97ec1698982b10136f66b34ffa9035f42ea3ba12
NadegeL <nadege.luthier@gmai.com>
2024-11-01 10:56:57 +0100

    try/except et gestion nb argv

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    if len(argv) != 5:
+        print("Usage : {} username password database \
+    state_name".format(argv[0]))
+        exit(1)
+
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+                    ORDER BY states.id ASC",(argv[4],))
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQLdb Error: {}".format(e))
+
+    finally:
+        if 'cur' in locals():
+            cur.close()
+
+        if 'db' in locals():
+            db.close()
