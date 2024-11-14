8eb13199b20b2f245c6a27fea627fedb87996a57
NadegeL <nadege.luthier@gmai.com>
2024-11-14 12:52:00 +0100

    galère avec password mysql

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,28 +6,39 @@ where name matches the argument.
 But this time, write one that is safe from MySQL injections!
     Exécuter une requête paramétrée pour éviter les injections SQL
     Connexion à la base de données MySQL
-
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-
-    db = MySQLdb.connect(
+def main():
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+    conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        charset="utf8"
     )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
-                ORDER BY states.id ASC", (argv[4],))
-    rows = cur.fetchall()
-
-    for row in rows:
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM states
+        WHERE name = %s
+        ORDER BY states.id ASC
+        """,
+        (state_name_searched, )
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-
     cur.close()
-    db.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,39 +1,37 @@
 #!/usr/bin/python3
 """ lists all cities from the database hbtn_0e_4_usa"""
 
-import MySQLdb
+import mysql.connector
 from sys import argv
 
-if __name__ == '__main__':
-    if len(argv) != 4:
-        print("Usage: {} <mysql username> <mysql password> \
-<database name>".format(argv[0]))
-        exit(1)
 
-    try:
+def main():
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    conn = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        database=database_name,
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+        """
+    )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
 
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-        )
 
-        cur = db.cursor()
-
-        cur.execute("""
-                SELECT cities.id, cities.name,
-                states.name FROM cities
-                JOIN states ON cities.state_id = states.id
-                ORDER BY cities.id ASC
-                """)
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error : {}".format(e))
-
-        cur.close()
-        db.close()
+if __name__ == "__main__":
+    main()
